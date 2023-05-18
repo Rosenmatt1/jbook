2d0d6f6e9e28f1e3870c6d820d8caa7f3128bcee
Matthew Rosenberg <rosenmatt1@gmail.com>
2023-05-18 00:38:51 -0600

    Made the fetch its own file

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,7 @@ import * as esbuild from 'esbuild-wasm';
 import { useState, useEffect, useRef } from 'react';
 import ReactDOM from 'react-dom';
 import { unpkgPathPlugin } from './plugins/unpkg-path-plugin';
+import { fetchPlugin } from './plugins/fetch-plugin';
 
 const App = () => {
   const ref = useRef<any>();
@@ -22,14 +23,15 @@ const App = () => {
       entryPoints: ['index.js'],
       bundle: true,
       write: false,
-      plugins: [unpkgPathPlugin(input)],
+      plugins: [
+        unpkgPathPlugin(),
+        fetchPlugin(input)
+      ],
       define: {
         'process.env.NODE_ENV': '"production"',
         global: 'window'
       }
     });
-
-    console.log(result)
 
     setCode(result.outputFiles[0].text)
   }
@@ -44,7 +46,6 @@ const App = () => {
       wasmURL: '/esbuild.wasm'
     })
   }
-
 
   return <div>
     <h1> Transpiler App </h1>
