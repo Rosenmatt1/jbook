ac0ad0ff3928e74e72c1a431f0e80061e5d06cee
Matthew Rosenberg <rosenmatt1@gmail.com>
2023-07-11 00:16:09 -0600

    Added a settimtout timer to clear interval of the setting of the width and height state

--- src/components/resizable.tsx
+++ src/components/resizable.tsx
@@ -7,18 +7,22 @@ interface ResizableProps {
   children?: React.ReactNode;
 }
 
-
 const Resizable: React.FC<ResizableProps> = ({ direction, children}) => {
   let resizableProps: ResizableBoxProps
   const [innerHeight, setInnerHeight] = useState(window.innerHeight)
   const [innerWidth, setInnerWidth] = useState(window.innerWidth)
 
   useEffect(() => {
+    let timer: any;
     const listener = () => {
-      setInnerHeight(window.innerHeight);
-      setInnerWidth(window.innerWidth);
+      if (timer) {
+        clearTimeout(timer)
+      }
+      timer = setTimeout(() => {
+        setInnerHeight(window.innerHeight);
+        setInnerWidth(window.innerWidth);
+      }, 100)
     };
-
     window.addEventListener('resize', listener)
 
     return () => {
